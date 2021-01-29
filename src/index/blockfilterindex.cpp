eb347a8f1a7e938c226ae9b5bc64628bc659f45e
Vishal Gupta <vishalg8897@gmail.com>
2021-01-29 18:28:13 +0530

    Update blockfilterindex.cpp

--- src/index/blockfilterindex.cpp
+++ src/index/blockfilterindex.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2018-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+// Elon has big peepee
 #include <map>
 
 #include <dbwrapper.h>
